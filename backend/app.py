1229c6cf27f9d4393cd9315ef4172bf765b918fa
dexterHK <dexterhk01@gmail.com>
2025-06-23 16:06:17 +0200

    fix dns

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ def create_app():
             "origins": [
                 "http://localhost:8080", "http://127.0.0.1:8080", "http://0.0.0.0:8080",
                 "http://localhost:5000", "http://127.0.0.1:5000", "http://0.0.0.0:5000",
-                "http://localhost:3000", "http://127.0.0.1:3000"
+                "http://139.59.213.46:8080", "http://139.59.213.46:5000"
             ],
             "methods": ["GET", "POST", "OPTIONS"],
             "allow_headers": ["Content-Type"]
